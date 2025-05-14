dec2a98e8ac62b5c3dd82649f5bd98e62f04af32
Quoc Hau <tranlequochau.blc@gmail.com>
2025-05-15 01:50:17 +0700

    add middleware nextjs

--- ui/middleware.ts
+++ ui/middleware.ts
@@ -1,40 +1,21 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { authRoutes } from "./constants/authRoute";
+import { decodeToken } from "./libs/utils";
 
-// // const { auth } = NextAuth(authConfig);
-// export default auth(async function middleware(req: NextRequest) {
-//   const { nextUrl } = req;
-//   const session = await auth();
-//   const userRole = session?.user?.role;
-//   const isLoggedIn = !!session;
-//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-//   const IsSalesRoute = salesRoutes.includes(nextUrl.pathname);
-//   const IsAuthRoute = authRoutes.includes(nextUrl.pathname);
-//   const isAdminRoute = nextUrl.pathname.startsWith(adminRoutes);
-//   if (isApiAuthRoute) {
-//     return NextResponse.next();
-//   }
-
-//   if (isLoggedIn) {
-//     if (IsAuthRoute) {
-//       return NextResponse.redirect(
-//         new URL(DEFAULT_LOGIN_REDIRECT, nextUrl.origin),
-//       );
-//     }
-//     return NextResponse.next();
-//   }
-//   if (isAdminRoute) {
-//     if (!isLoggedIn || userRole === "USER") {
-//       return NextResponse.redirect(new URL("/login", nextUrl));
-//     }
-//   }
-
-//   if (!isLoggedIn && IsSalesRoute) {
-//     return NextResponse.redirect(new URL("/login", nextUrl));
-//   }
-//   return NextResponse.next();
-// });
-
-export default async function middleware(req: NextRequest) {}
+export default async function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const decodedToken = decodeToken(accessToken as string);
+  const role = decodedToken?.roleName;
+  const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+  const IsAuthRoute = authRoutes.includes(req.nextUrl.pathname);
+  if (isAdminRoute && role !== "ADMIN") {
+    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+  }
+  if (IsAuthRoute && accessToken) {
+    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+  }
+  return NextResponse.next();
+}
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
