0ba8abd858a4316c7598f74e391441cad3dc7557
Quoc Hau <tranlequochau.blc@gmail.com>
2025-06-08 16:22:16 +0700

    big update 3

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -6,7 +6,7 @@ import Dock from "@/components/layout/Dock/page";
 import Footer from "@/components/layout/Footer/page";
 import { Header } from "@/components/layout/Header/page";
 import ContactSection from "@/components/layout/Home/contact";
-import HeroSection from "@/components/layout/Home/hero";
+import HeroSection from "@/components/layout/Home/hero-section";
 import ProjectsSection from "@/components/layout/Home/projects";
 import SkillsSection from "@/components/layout/Home/skills";
 import ScrollLinked from "@/components/ui/scroll-linked";
